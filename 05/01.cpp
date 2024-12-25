91398a34e7b2d94910b62145de148656e68ce6e3
Philipp Lenk <philipp.lenk@codemetas.de>
2024-12-25 15:11:48 +0100

    Day 5 :)

---
+++ 05/01.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+int main(int argc, char* argv[])
+{
+	std::unordered_map<int, std::unordered_set<int>> after;
+	for(std::string line; std::getline(std::cin, line) && !line.empty();)
+	{
+		std::stringstream strm{line};
+		int first, second;
+		char skip;
+		strm>>first>>skip>>second;
+
+		after[first].insert(second);
+	}
+
+	const auto ordered_before = [&](const auto& lhs, const auto& rhs)
+	{
+		return after[lhs].contains(rhs);
+	};
+
+	int sum = 0;
+	for(std::string line; std::getline(std::cin, line);)
+	{
+		std::stringstream strm{line};
+
+		std::vector<int> values;
+		for(int value; strm>>value; strm.get())
+			values.push_back(value);
+
+		if(std::ranges::is_sorted(values, ordered_before))
+			sum+=values[values.size()/2];
+	}
+
+	std::cout<<sum;
+	return 0;
+}
