7b1ef3a9c71938ad4b80a113b28861078438a2e9
Philipp Lenk <philipp.lenk@codemetas.de>
2024-12-25 15:17:34 +0100

    Day 14 :)

---
+++ 14/02.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+struct point2d
+{
+	int x, y;
+};
+
+struct vec2d
+{
+	int x, y;
+};
+
+point2d operator+(const point2d& lhs, const vec2d& rhs)
+{
+	return {lhs.x+rhs.x, lhs.y+rhs.y};
+}
+
+struct robot
+{
+	point2d position;
+	vec2d velocity;
+};
+
+std::istream& operator>>(std::istream& in, robot& r)
+{
+	char trash_c;
+	return in>>trash_c>>trash_c>>r.position.x>>trash_c>>r.position.y>>trash_c>>trash_c>>r.velocity.x>>trash_c>>r.velocity.y;
+}
+
+int main(int argc, char* argv[])
+{
+	constexpr auto width = 101;
+	constexpr auto height = 103;
+	
+	std::vector<robot> robots{std::istream_iterator<robot>{std::cin},{}};
+
+	for(int i=0;;++i)
+	{
+		for(auto& r: robots)
+		{
+			r.position = r.position+r.velocity;
+			r.position.x = (r.position.x%width+width)%width;
+			r.position.y = (r.position.y%height+height)%height;
+		}
+
+
+		int q0 = 0, q1= 0, q2 = 0, q3 = 0;
+		for(const auto& r: robots)
+		{
+			if(r.position.x<width/2 && r.position.y<height/2) ++q0;
+			if(r.position.x<width/2 && r.position.y>height/2) ++q1;
+			if(r.position.x>width/2 && r.position.y<height/2) ++q2;
+			if(r.position.x>width/2 && r.position.y>height/2) ++q3;
+		}
+
+		const auto most_popular_quadrant = std::max({q0,q1,q2,q3});
+		if(most_popular_quadrant>q0+q1+q2+q3-most_popular_quadrant)
+		{
+			std::vector<char> grid(width*height, '.');
+			for(const auto& r: robots)
+				grid[r.position.y*width+r.position.x] = '#';
+
+			std::cout<<i<<":\n";
+			for(int y=0; y<height; ++y)
+			{
+				for(int x=0; x<width; ++x)
+					std::cout<<grid[y*width+x];
+
+				std::cout<<'\n';
+			}
+			break;
+		}
+	}
+
+	return 0;
+}
